ac0639ca888730f7aa676a18848452845c94aa5d
Ruta <ruta@baltiejus.com>
2016-05-11 12:11:34 +0100

    airport has no planes by default failing

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -1,6 +1,11 @@
-// 'use strict';
-//
-// describe('Airport', function() {
-//
-//   describe('Landing a plane')
-// });
+'use strict';
+
+describe('Airport', function() {
+  var airport;
+  beforeEach(function(){
+    airport = new Airport();
+  });
+  it('has no planes by default', function(){
+    expect(airport.planes()).toEqual([]);
+  });
+});
--- spec/PlaneSpec.js
+++ spec/PlaneSpec.js
@@ -1,11 +1,11 @@
 'use strict';
 
-describe('Plane', function() {
+describe('Plane',function(){
   var plane;
-  beforeEach(function() {
+  beforeEach(function(){
     plane = new Plane();
   });
-  describe('It can land in an airport', function() {
-    expect(plane.land).not.toBeUndefined();
+  it('It can land in an airport', function(){
+    expect(plane.land).not.toBeUndefined()
   });
 });
